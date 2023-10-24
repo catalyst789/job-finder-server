a471ec7cf9767a1c1b09d32d4a045aef2cb4ec3f
Jalaj Kumar Gaur <raajk0338@gmail.com>
2023-10-24 21:33:50 +0530

    feat: getAllJobApplications based on specific job Id

--- src/controllers/JobController.ts
+++ src/controllers/JobController.ts
@@ -42,7 +42,22 @@ export const applyJob = async (req: Request, res: Response) => {
     });
     res.status(200).json({ message: "Sucessfully applied for this job" });
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
+export const getJobApplicationsJobId = async (req: Request, res: Response) => {
+  try {
+    const jobId = req.params.jobId;
+    const jobApplications = await JobApplicantModel.find({ jobId });
+    // const jobApplications = await JobApplicantModel.find().where('jobId').equals(jobId);
+    if (!jobApplications)
+      return res
+        .status(404)
+        .json({ message: "No Applications Found for this id" });
+    res.status(200).json(jobApplications);
+  } catch (error) {
     res.status(500).send(error);
   }
 };
--- src/routes/JobRoutes.ts
+++ src/routes/JobRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { applyJob, getAllJobs, getJobById } from "../controllers/JobController";
+import { applyJob, getAllJobs, getJobApplicationsJobId, getJobById } from "../controllers/JobController";
 
 const router = express();
 
 router.get("/getAll", getAllJobs);
 router.get("/get/:jobId", getJobById);
 router.post("/apply", applyJob);
+router.get("/getJobApplicationsById/:jobId", getJobApplicationsJobId);
 
 
 module.exports = router;
